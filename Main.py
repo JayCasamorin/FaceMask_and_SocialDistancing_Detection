6651d48d3824996bdca54415f1545ec7986d95bb
JayCasamorin <45162210+JayCasamorin@users.noreply.github.com>
2022-02-10 16:28:10 +0800

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,3 +1,8 @@
+# USAGE
+# python Main.py --input http://192.168.68.110:8080/video
+
+# import the necessary packages
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.models import load_model
@@ -15,12 +20,14 @@ import os
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", type=str, default="",
+				help="optional path to video file")
 ap.add_argument("-f", "--face", type=str, default="face_detector",
-                help="path to face detector model directory")
+				help="path to face detector model directory")
 ap.add_argument("-m", "--model", type=str, default="mask_detector.model",
-                help="path to trained face mask detector model")
+				help="path to trained face mask detector model")
 ap.add_argument("-c", "--confidence", type=float, default=0.5,
-                help="minimum probability to filter weak detections")
+				help="minimum probability to filter weak detections")
 args = vars(ap.parse_args())
 
 # load our serialized face detector model from disk
@@ -45,107 +52,111 @@ configPath = os.path.sep.join([config.MODEL_PATH, "yolov3.cfg"])
 print("[INFO] loading YOLO from disk...")
 net = cv2.dnn.readNetFromDarknet(configPath, distWeightsPath)
 
-if config.USE_GPU:
-    # set CUDA as the preferable backend and target
-    print("[INFO] setting preferable backend and target to CUDA...")
-    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
-
 # determine only the *output* layer names that we need from YOLO
 ln = net.getLayerNames()
 ln = [ln[i - 1] for i in net.getUnconnectedOutLayers()]
 
 # initialize the video stream
 print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
+vs = cv2.VideoCapture(args["input"] if args["input"] else 0)
 
-# loop over the frames from the video stream
+# loop over frames from the video stream
 while True:
-    # grab the frame from the threaded video stream and resize it
-    frame = vs.read()
-    frame = imutils.resize(frame, width=600)
+	# read a frame from the video stream
+	(grabbed, frame) = vs.read()
 
-    # detect faces in the frame and determine if they are wearing a face mask or not
-    (locs, preds) = md.detect_and_predict_mask(frame, faceNet, maskNet)
+	# if the frame was not grabbed then we've reached the end of
+	# the video stream so exit the script
+	if not grabbed:
+		print("[INFO] no frame read from stream - exiting")
+		break
 
-    # detect people (and only people) in it
-    results = sdd.detect_people(frame, net, ln,
-                            personIdx=LABELS.index("person"))
+	# grab the frame from the threaded video stream and resize it
+	frame = imutils.resize(frame, width=600)
 
-    # initialize the set of indexes that violate the minimum social distance
-    violate = set()
+	# detect faces in the frame and determine if they are wearing a face mask or not
+	(locs, preds) = md.detect_and_predict_mask(frame, faceNet, maskNet)
 
-    # ensure there are *at least* two people detections (required in
-    # order to compute our pairwise distance maps)
-    if len(results) >= 2:
-        # extract all centroids from the results and compute the
-        # Euclidean distances between all pairs of the centroids
-        centroids = np.array([r[2] for r in results])
-        D = dist.cdist(centroids, centroids, metric="euclidean")
+	# detect people (and only people) in it
+	results = sdd.detect_people(frame, net, ln,
+								personIdx=LABELS.index("person"))
 
-        # loop over the upper triangular of the distance matrix
-        for i in range(0, D.shape[0]):
-            for j in range(i + 1, D.shape[1]):
-                # check to see if the distance between any two
-                # centroid pairs is less than the configured number
-                # of pixels
-                if D[i, j] < config.MIN_DISTANCE:
-                    # update our violation set with the indexes of
-                    # the centroid pairs
-                    violate.add(i)
-                    violate.add(j)
+	# initialize the set of indexes that violate the minimum social distance
+	violate = set()
 
-        # loop over the results
-        for (i, (prob, bbox, centroid)) in enumerate(results):
-            # extract the bounding box and centroid coordinates, then
-            # initialize the color of the annotation
-            (startX, startY, endX, endY) = bbox
-            (cX, cY) = centroid
-            color = (0, 255, 0)
+	# ensure there are *at least* two people detections (required in
+	# order to compute our pairwise distance maps)
+	if len(results) >= 2:
+		# extract all centroids from the results and compute the
+		# Euclidean distances between all pairs of the centroids
+		centroids = np.array([r[2] for r in results])
+		D = dist.cdist(centroids, centroids, metric="euclidean")
 
-            # if the index pair exists within the violation set, then
-            # update the color
-            if i in violate:
-                color = (0, 0, 255)
+		# loop over the upper triangular of the distance matrix
+		for i in range(0, D.shape[0]):
+			for j in range(i + 1, D.shape[1]):
+				# check to see if the distance between any two
+				# centroid pairs is less than the configured number
+				# of pixels
+				if D[i, j] < config.MIN_DISTANCE:
+					# update our violation set with the indexes of
+					# the centroid pairs
+					violate.add(i)
+					violate.add(j)
 
-            # draw (1) a bounding box around the person and (2) the
-            # centroid coordinates of the person,
-            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
-            cv2.circle(frame, (cX, cY), 5, color, 1)
+		# loop over the results
+		for (i, (prob, bbox, centroid)) in enumerate(results):
+			# extract the bounding box and centroid coordinates, then
+			# initialize the color of the annotation
+			(startX, startY, endX, endY) = bbox
+			(cX, cY) = centroid
+			color = (0, 255, 0)
 
-    # loop over the detected face locations and their corresponding
-    # locations
-    for (box, pred) in zip(locs, preds):
-        # unpack the bounding box and predictions
-        (startX, startY, endX, endY) = box
-        (mask, withoutMask) = pred
+			# if the index pair exists within the violation set, then
+			# update the color
+			if i in violate:
+				color = (0, 0, 255)
 
-        # determine the class label and color we'll use to draw
-        # the bounding box and text
-        label = "Mask" if mask > withoutMask else "No Mask"
-        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+			# draw (1) a bounding box around the person and (2) the
+			# centroid coordinates of the person,
+			cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+			cv2.circle(frame, (cX, cY), 5, color, 1)
 
-        # include the probability in the label
-        label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+	# loop over the detected face locations and their corresponding
+	# locations
+	for (box, pred) in zip(locs, preds):
+		# unpack the bounding box and predictions
+		(startX, startY, endX, endY) = box
+		(mask, withoutMask) = pred
 
-        # display the label and bounding box rectangle on the output
-        # frame
-        cv2.putText(frame, label, (startX, startY - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+		# determine the class label and color we'll use to draw
+		# the bounding box and text
+		label = "Mask" if mask > withoutMask else "No Mask"
+		color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
 
-    # draw the total number of social distancing violations on the
-    # output frame
-    text = "Social Distancing Violations: {}".format(len(violate))
-    cv2.putText(frame, text, (10, frame.shape[0] - 25),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.85, (0, 0, 255), 3)
+		# include the probability in the label
+		label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
 
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1)
+		# display the label and bounding box rectangle on the output
+		# frame
+		cv2.putText(frame, label, (startX, startY - 10),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+		cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 
-    if key == ord("q"):
-        break
+	# draw the total number of social distancing violations on the
+	# output frame
+	text = "Social Distancing Violations: {}".format(len(violate))
+	cv2.putText(frame, text, (10, frame.shape[0] - 25),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.85, (0, 0, 255), 3)
+
+	# show the output to our screen
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
 
 # do cleanup
 cv2.destroyAllWindows()
-vs.stop()
+vs.release()
